2fa217ddabfdf0f6b2581ff4ae69136511196a27
TimGuiteDiamond <tim.guite@diamond.ac.uk>
2019-05-13 16:26:23 +0100

    Initial phase to mtz conversion now possible and have tested phs to mtz to map pipeline for example structure.

--- dls_topaz3/conversions.py
+++ dls_topaz3/conversions.py
@@ -7,13 +7,69 @@ import logging
 
 from pathlib import Path
 
-def phs_to_mtz():
-    """Use the CCP4 f2mtz utility to convert files"""
+def phs_to_mtz(phase_filename, output_filename, cell_info, symmetry_group):
+    """Use the CCP4 f2mtz utility to convert a phase file into a .mtz file and return the new file location."""
+    try:
+        phase_filepath = Path(phase_filename)
+        output_filepath = Path(output_filename)
+    except:
+        raise Exception("Inputs must be paths of input phase file and output mtz file.")
 
-    result = procrunner.run(['f2mtz', "hklin", "/dls/mx-scratch/melanie/for_METRIX/results_20190326/AI_training/topaz_test/EP_phasing/traced/4PUC/P212121/4PUC.phs", "hklout", "test_mtz_python.mtz"], stdin="CELL 66.45 112.123 149.896 90 90 90\nSYMM 19\nlabout H K L F FOM PHI SGF\nCTYPOUT H H H F W P Q\n", print_stdout=False, print_stderr=False)
+    logging.info(f"Converting phase to mtz")
+    logging.info(f"Input file at {phase_filepath}")
+    logging.info(f"Output file at {output_filepath}")
+
+    # Check parameters coming in
+    assert phase_filepath.suffix == ".phs" or phase_filepath.suffix == ".pha", \
+        "Please provide a phase_filename which points to the phase file you wish to convert."
+    assert phase_filepath.exists(), f"Could not find a valid file at {phase_filepath}."
+    assert output_filepath.suffix == ".mtz", "Please provide an output_filename with a .mtz extension."
+    assert output_filepath.parent.exists(), f"Could not find output directory at {output_filepath.parent}"
+    try:
+        assert len(cell_info) == 6
+        # Will raise error if values are not floats
+        test_float = [float(value) for value in cell_info]
+    except:
+        raise Exception("Cell info must be list of 6 floating numbers for the cell lengths and angles.")
+    assert type(symmetry_group) == int, "Please provide an integer symmetry group for the transformation."
+
+    # Convert list of cells info into string
+    cell_info_string = " ".join([str(cell) for cell in cell_info])
+
+    # Build up keywords
+    keywords = "\n".join([
+        f"CELL {cell_info_string}",
+        f"SYMM {symmetry_group}",
+        "labout H K L F FOM PHI SIGF",
+        "CTYPOUT H H H F W P Q"
+    ])
+
+    logging.info(f"Keywords: {keywords}")
+
+    # Convert to bytes
+    b_keywords = bytes(keywords, "utf-8")
+
+    # Get location of shell script
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    cfft_shell = os.path.join(__location__, 'shell_scripts/f2mtz.sh')
+
+    # Build up command list
+    command = [cfft_shell, "hklin", phase_filepath, "hklout", output_filepath]
+
+    logging.info(f"Command: {command}")
+
+    result = procrunner.run(command, stdin=b_keywords)
+
+    #Check that it worked
+    assert result["exitcode"] == 0, f"Error converting {phase_filepath} to {output_filepath}"
+    assert result["stderr"] == b"", f"Error collecting information from {phase_filepath} to {output_filepath}"
+
+    logging.info("Conversion successful")
+
+    return output_filepath
 
 def mtz_to_map(mtz_filename, output_filename):
-    """Convert .mtz file to map using cfft utility"""
+    """Convert .mtz file to map using cfft utility and return the new output file location."""
 
     try:
         mtz_filepath = Path(mtz_filename)
@@ -26,9 +82,10 @@ def mtz_to_map(mtz_filename, output_filename):
     logging.info(f"Output file at {output_filepath}")
 
     # Check parameters coming in
-    assert mtz_filepath.suffix == ".mtz", "Please provide an mtz_filename which points to the .mtz file you wish to convert"
+    assert mtz_filepath.suffix == ".mtz",\
+        "Please provide an mtz_filename which points to the .mtz file you wish to convert."
     assert mtz_filepath.exists(), f"Could not find a valid file at {mtz_filepath}"
-    assert output_filepath.suffix == ".map", "Please provide an output_filename with a .map extension"
+    assert output_filepath.suffix == ".map", "Please provide an output_filename with a .map extension."
     assert output_filepath.parent.exists(), f"Could not find output directory at {output_filepath.parent}"
 
     # Build up standard input for cfft utility
@@ -40,9 +97,10 @@ def mtz_to_map(mtz_filename, output_filename):
         "stats-radius 4.0"
     ])
 
-    b_keywords = bytes(keywords, "utf-8")
-
     logging.info(f"Keywords: {keywords}")
+
+    # Convert to bytes
+    b_keywords = bytes(keywords, "utf-8")
 
     # Get location of shell script
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -60,6 +118,10 @@ def mtz_to_map(mtz_filename, output_filename):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+
+    phs_to_mtz('/dls/science/users/riw56156/topaz_test_data/python_test/4PUC.phs',
+               '/dls/science/users/riw56156/topaz_test_data/python_test/4PUC.mtz',
+               [66.45, 112.123, 149.896, 90, 90, 90], 19)
 
     mtz_to_map('/dls/science/users/riw56156/topaz_test_data/python_test/4PUC.mtz',
                '/dls/science/users/riw56156/topaz_test_data/python_test/4PUC.map')
